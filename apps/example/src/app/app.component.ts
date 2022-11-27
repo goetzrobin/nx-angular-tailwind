1869d398525b8fdb1e32a6bffe3773a9b8a1d131
Robin Goetz <tug29225@temple.edu>
2022-11-27 14:35:03 -0500

    feat: add tailwind config to example app

--- apps/example/src/app/app.component.ts
+++ apps/example/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nat-root',
-  template: `<p>Hello world</p>`,
+  template: `<p class="font-medium">Hello world</p>`,
   styles: []
 })
 export class AppComponent {
