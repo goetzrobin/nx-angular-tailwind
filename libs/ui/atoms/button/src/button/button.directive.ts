632656a478215c6654d6c083fadbd21f1ef38ecf
Robin Goetz <tug29225@temple.edu>
2022-11-27 19:48:45 -0500

    feat: add tw styles based on functionality to button directive

--- libs/ui/atoms/button/src/button/button.directive.ts
+++ libs/ui/atoms/button/src/button/button.directive.ts
@@ -1,10 +1,14 @@
 import {Directive, HostBinding} from "@angular/core";
 
+const base = 'inline-flex items-center rounded-md border font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2'
+const size = 'px-4 py-2 text-sm'
+const theme = 'border-transparent bg-sky-600 text-white hover:bg-sky-700 focus:ring-sky-500'
+
 @Directive({
   selector: '[natButton]',
   standalone: true,
 })
 export class ButtonDirective {
   @HostBinding('class')
-  private twClasses = 'bg-sky-500'
+  private twClasses = `${base} ${size} ${theme}`
 }
