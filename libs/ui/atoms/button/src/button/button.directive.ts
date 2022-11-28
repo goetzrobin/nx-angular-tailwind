f5c926e138d741751407dc3e7df1ba03d4f5c3e0
Robin Goetz <tug29225@temple.edu>
2022-11-27 21:26:16 -0500

    feat: add size input to button directive & refactor it to use config objects and input keys to build tw classes & add disabled styles

--- libs/ui/atoms/button/src/button/button.directive.ts
+++ libs/ui/atoms/button/src/button/button.directive.ts
@@ -1,14 +1,25 @@
 import {Directive, HostBinding, Input} from "@angular/core";
 
-const base = 'inline-flex items-center rounded-md border font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2'
-const size = 'px-4 py-2 text-sm'
-const theme = 'border-transparent bg-sky-600 text-white hover:bg-sky-700 focus:ring-sky-500'
-const themeSecondary = 'border-gray-300 bg-white text-gray-700 hover:bg-sky-700 focus:ring-gray-50'
+const base = `inline-flex items-center rounded-md border font-medium shadow-sm
+  focus:outline-none focus:ring-2 focus:ring-offset-2
+  disabled:opacity-50 disabled:focus:ring-0 disabled:active:ring-0`;
 
-export type Theme = 'primary' | 'secondary';
+export const themes = ['primary', 'secondary'] as const;
+export type Theme = typeof themes[number];
+const themeClasses: { [key in Theme]: string } = {
+  primary: 'border-transparent bg-sky-600 text-white hover:bg-sky-700 disabled:bg-sky-600 focus:ring-sky-500',
+  secondary: 'border-gray-300 bg-white text-gray-700 hover:bg-gray-50 disabled:bg-white focus:ring-sky-500'
+}
 
-const buildTwClasses = (currentTheme: Theme): string =>
-  `${base} ${size} ${currentTheme === 'secondary' ? themeSecondary : theme}`;
+export const sizes = ['base', 'l'] as const;
+export type Size = typeof sizes[number];
+const sizeClasses: { [key in Size]: string } = {
+  base: 'px-4 py-2 text-sm',
+  l: 'px-5 py-3 text-base'
+};
+
+const buildTwClasses = (currentTheme: Theme, size: Size): string =>
+  `${base} ${sizeClasses[size]} ${themeClasses[currentTheme]}`;
 
 @Directive({
   selector: '[natButton]',
@@ -16,12 +27,19 @@ const buildTwClasses = (currentTheme: Theme): string =>
 })
 export class ButtonDirective {
   @HostBinding('class')
-  private twClasses = buildTwClasses('primary')
+  private twClasses = buildTwClasses('primary', 'base');
 
   private _theme: Theme = 'primary';
   @Input()
   set theme(value: Theme) {
     this._theme = value;
-    this.twClasses = buildTwClasses(this._theme);
+    this.twClasses = buildTwClasses(this._theme, this._size);
+  }
+
+  private _size: Size = 'base';
+  @Input()
+  set size(value: Size) {
+    this._size = value;
+    this.twClasses = buildTwClasses(this._theme, this._size);
   }
 }
--- libs/ui/atoms/button/src/button/button.stories.ts
+++ libs/ui/atoms/button/src/button/button.stories.ts
@@ -1,13 +1,44 @@
-import {ButtonDirective} from "./button.directive";
+import {ButtonDirective, sizes, themes} from "./button.directive";
 import {Meta, Story} from "@storybook/angular";
 
+type ButtonDirectiveProps = ButtonDirective & {
+  content: string;
+};
 export default {
   title: 'Design System/Atoms/Button',
-} as Meta<ButtonDirective>;
+  argTypes: {
+    size: {
+      control: { type: 'select' },
+      options: sizes,
+    },
+    theme: {
+      control: { type: 'select' },
+      options: themes,
+    },
+  },
+} as Meta<ButtonDirectiveProps>;
 
-export const Default: Story<ButtonDirective> = () => ({
+const DefaultArgs = {size: sizes[0], theme: themes[0], content: 'Angular & Tailwind rock'};
+
+const Template: Story<ButtonDirectiveProps & {  disabled: boolean; }> = (args) => ({
+  props: args,
   moduleMetadata: {
     imports: [ButtonDirective],
   },
-  template: `<button natButton theme="secondary">Click</button>`
+  template: `<button natButton [disabled]="disabled" [size]="size" [theme]="theme">{{content}}</button>`
 });
+
+export const Default = Template.bind({})
+Default.args = {...DefaultArgs, disabled: false};
+
+
+const AnchorTemplate: Story<ButtonDirectiveProps & {link: string}> = (args) => ({
+  props: args,
+  moduleMetadata: {
+    imports: [ButtonDirective],
+  },
+  template: `<a target="_blank" [href]="link" natButton [size]="size" [theme]="theme">{{content}}</a>`
+});
+
+export const Anchor = AnchorTemplate.bind({})
+Anchor.args = {...DefaultArgs, link: 'https://media.giphy.com/media/jJQC2puVZpTMO4vUs0/giphy.gif'};
